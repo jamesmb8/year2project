3e33b9c72b3223d239d6c2ac5e3f1935146b9bf2
jamesmb8 <77098752+jamesmb8@users.noreply.github.com>
2024-11-08 10:51:29 +0000

    Co-authored-by: James-Hibbert <James-Hibbert@users.noreply.github.com> Co-authored-by: Declan Clifford- Johnson <Dec-246@users.noreply.github.com> Co-authored-by: Sebbychelsea10 <Sebbychelsea10@users.noreply.github.com>

---
+++ php/managernav.php
@@ -0,0 +1,5 @@
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="account.html">Account</a>
+    <a href="reports.html">Reports</a>
+</div>
---
+++ php/usernav.php
@@ -0,0 +1,4 @@
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="account.html">Account</a>
+</div>
