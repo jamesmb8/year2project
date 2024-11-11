101329a65ab20f0470fdc4cea1f00e752586c447
jamesmb8 <77098752+jamesmb8@users.noreply.github.com>
2024-11-11 16:02:18 +0000

    done register

--- php/register.php
+++ php/register.php
@@ -13,6 +13,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection Failed");
 }
+else {
+    echo("Successfull");
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
