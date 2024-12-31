26a6a4251182fee3631c27f211c8855d71791f09
Dec-246 <Declan.Clifford-Johnson@student.shu.ac.uk>
2024-12-31 00:38:03 +0000

    attempt at creating closing navbar

--- php/sidenav.php
+++ php/sidenav.php
@@ -2,14 +2,12 @@
 session_start();
 $role = $_SESSION['role'] ?? 'customer';
 ?>
+
   <link rel="stylesheet" href="../CSS/style.css" />
+
 <div class="sidenav" id="sidenav">
-
-    <a href="dashboard.php">Home</a>
-    <a href="account.php">Account</a>
-    <a id="AboutUs" href="about.php">About Us</a>
-   
-    
-
+  <a href="dashboard.php">Home</a>
+  <a href="account.php">Account</a>
+  <a id="AboutUs" href="about.php">About Us</a>
 </div>
 
